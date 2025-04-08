0cc6fb064ba3587b4ea0dc5e1c8ffe78a46b5914
Angelo-Pio <angelopompeo0@gmail.com>
2025-04-08 17:15:57 +0200

    Code adpted to create temperature chart

--- cloud-application/sapienza/src/main/java/com/sapienza/controller/DashboardController.java
+++ cloud-application/sapienza/src/main/java/com/sapienza/controller/DashboardController.java
@@ -2,21 +2,21 @@ package com.sapienza.controller;
 
 import com.sapienza.subscription.SubscriptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DashboardController{
 
     @Autowired
     SubscriptionHandler subscriptionHandler;
 
-    @GetMapping( "/hello")
-    public String index() {
+    @GetMapping( "/")
+    public String index(Model model) {
 
-        subscriptionHandler.store("df","12");
-        System.out.println("hello");
-        return "Hello World";
+        return "realtime";
     }
 
 }
