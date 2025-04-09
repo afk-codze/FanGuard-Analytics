0667e303d23b84903558f7f9216897de6963f5cb
Angelo-Pio <angelopompeo0@gmail.com>
2025-04-09 13:45:16 +0200

    Now realtime graph fetches only last 2 hours data, should adapt how timestamp is represented in the graph (timestamp is too long)

--- cloud-application/sapienza/src/main/java/com/sapienza/controller/RestApiController.java
+++ cloud-application/sapienza/src/main/java/com/sapienza/controller/RestApiController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController()
@@ -21,7 +22,10 @@ public class RestApiController {
 
     @GetMapping("/api/datastream/all")
     public  List<Datastream> getAllDatastreams() {
-        return datastreamRepository.findAllByOrderByTimestampAsc();
+//        return datastreamRepository.findAllByOrderByTimestampAsc();
+
+        //returns all data in the last 2 hours
+        return datastreamRepository.findByTimestampGreaterThanOrderByTimestampAsc(LocalDateTime.now().minusHours(2));
     }
 
     @GetMapping("/api/anomalies")
--- cloud-application/sapienza/src/main/java/com/sapienza/repository/DatastreamRepository.java
+++ cloud-application/sapienza/src/main/java/com/sapienza/repository/DatastreamRepository.java
@@ -4,11 +4,13 @@ import com.sapienza.model.Datastream;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DatastreamRepository extends JpaRepository<Datastream, Long> {
 
-    List<Datastream> getAllById(Long id);
     List<Datastream> findAllByOrderByTimestampAsc();  // Ascending order}
+    List<Datastream> findByTimestampGreaterThanOrderByTimestampAsc(LocalDateTime timestamp);
+
 }
