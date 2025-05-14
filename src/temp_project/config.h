8c99a5710ba4d72e29c6f1d110d074038af90272
corniciluca <co.luca00@gmail.com>
2025-05-15 00:22:18 +0200

    add light sleep

---
+++ src/temp_project/config.h
@@ -0,0 +1,11 @@
+#pragma once
+#define WIFI_MAX_RETRIES 10
+#define MSG_BUFFER_SIZE 50
+#define RETRY_DELAY 1000 / portTICK_PERIOD_MS
+#define MQTT_LOOP portTICK_PERIOD_MS
+#define DATASTREAM_RMS_TOPIC "rms"
+#define ANOMALY_RMS_TOPIC "anomalie"
+#define PREF_SESSION_ID "session"
+
+#define QUEUE_SIZE 10
+
---
+++ src/temp_project/shared-defs.h
@@ -0,0 +1,31 @@
+#ifndef SHARED_DEFS_H
+#define SHARED_DEFS_H
+#include <Wire.h>
+#include <MPU6500_WE.h>
+#include <config.h>
+#define MPU6500_ADDR 0x68
+
+struct DATA_TO_SEND{
+  unsigned long time_stamp = 0;
+  float *rms_array = {0};
+  volatile bool anomaly = false;
+};
+
+typedef DATA_TO_SEND data_to_send_t;
+
+extern TaskHandle_t fft_sampling_task_handle;
+extern TaskHandle_t main_task_handle;
+extern TaskHandle_t communication_mqtt_task_handle;
+extern TaskHandle_t wifi_task_handle;
+
+extern QueueHandle_t xQueue_data;
+extern bool anomaly_detected;
+extern int id_device;
+extern bool anomaly_sent;
+extern bool this_reboot_send_rms;
+extern data_to_send_t anomaly;
+extern MPU6500_WE myMPU6500;
+extern bool g_is_wakeup_from_deep_sleep;
+
+void init_shared_queues();
+#endif
